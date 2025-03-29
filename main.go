1af6f091ffb37b60ae2babd39b694310af36c227
sunilpar <sunilformal42@gmail.com>
2025-03-29 04:32:44 +0000

    verdict:median is better on avg than low as pivot

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	//default is assending order
 	// arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
 	// arr1 := []int{80, 70, 60, 50, 40, 30, 20, 10}
 	arr1 := rand.Perm(10)
@@ -18,32 +17,10 @@ func main() {
 	//	fmt.Printf("sorted arr %v\n", arr1)
 	e := time.Since(s)
 
-	arr1 = rand.Perm(100)
-	s = time.Now()
-	Algo.QuickSort(arr1, 0, len(arr1)-1)
-	//	fmt.Printf("sorted  arr %v\n", arr1)
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
-
-	arr1 = rand.Perm(1000)
-	s = time.Now()
-	Algo.QuickSort(arr1, 0, len(arr1)-1)
-	//	fmt.Printf("sorted  arr %v\n", arr1)
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
-
 	arr1 = rand.Perm(10000)
 	s = time.Now()
 	Algo.QuickSort(arr1, 0, len(arr1)-1)
 	//	fmt.Printf("sorted  arr %v\n", arr1)
 	e = time.Since(s)
 	fmt.Printf("time taken:%v\n", e)
-
-	arr1 = rand.Perm(10000)
-	s = time.Now()
-	Algo.QuickSort(arr1, 0, len(arr1)-1)
-	//	fmt.Printf("sorted  arr %v\n", arr1)
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
-
 }
