d606c65ed389f5d2da5651f19a34f4ef0e298872
sunilpar <sunilformal42@gmail.com>
2025-03-29 04:26:36 +0000

    quicksort testing with low as pivot and median as pivot for 10 to 10,000 size arrays

--- Algorithms/QuickSort.go
+++ Algorithms/QuickSort.go
@@ -33,3 +33,31 @@ func QuickSort[T constraints.Ordered](arr []T, p, q int) {
 	}
 
 }
+
+func QuickSortlow[T constraints.Ordered](arr []T, p, q int) {
+	if p < q {
+		P := partitionlow(arr, p, q)
+		QuickSort(arr, p, P-1)
+		QuickSort(arr, P+1, q)
+	}
+
+}
+
+func partitionlow[T constraints.Ordered](arr []T, p, q int) int {
+	P := arr[p]
+	i := p
+	j := q
+	for i < j {
+		for arr[i] <= P && i <= q-1 {
+			i++
+		}
+		for arr[j] > P && j >= p+1 {
+			j--
+		}
+		if i < j {
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[p], arr[j] = arr[j], arr[p]
+	return j
+}
--- main.go
+++ main.go
@@ -3,17 +3,47 @@ package main
 import (
 	Algo "DSA/Algorithms"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
 	//default is assending order
-	arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
+	// arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
 	// arr1 := []int{80, 70, 60, 50, 40, 30, 20, 10}
+	arr1 := rand.Perm(10)
 
 	s := time.Now()
 	Algo.QuickSort(arr1, 0, len(arr1)-1)
 	//	fmt.Printf("sorted arr %v\n", arr1)
 	e := time.Since(s)
+
+	arr1 = rand.Perm(100)
+	s = time.Now()
+	Algo.QuickSort(arr1, 0, len(arr1)-1)
+	//	fmt.Printf("sorted  arr %v\n", arr1)
+	e = time.Since(s)
 	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = rand.Perm(1000)
+	s = time.Now()
+	Algo.QuickSort(arr1, 0, len(arr1)-1)
+	//	fmt.Printf("sorted  arr %v\n", arr1)
+	e = time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = rand.Perm(10000)
+	s = time.Now()
+	Algo.QuickSort(arr1, 0, len(arr1)-1)
+	//	fmt.Printf("sorted  arr %v\n", arr1)
+	e = time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = rand.Perm(10000)
+	s = time.Now()
+	Algo.QuickSort(arr1, 0, len(arr1)-1)
+	//	fmt.Printf("sorted  arr %v\n", arr1)
+	e = time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
 }
