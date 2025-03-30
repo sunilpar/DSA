f864048eb1ae32675509a0ce684f2c83a6a8a8c3
sunilpar <sunilformal42@gmail.com>
2025-03-30 01:15:16 +0000

    merge sort

---
+++ Algorithms/MergeSort.go
@@ -0,0 +1,14 @@
+package Algo
+
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func divide[T constraints.Ordered](arr []T, p, q int) int {
+
+	return 0
+}
+
+func MergeSort[T constraints.Ordered](arr []T, p, q int) {
+
+}
