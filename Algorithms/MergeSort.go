0946d31cf1db72af86367c91ac9a348fea7ebc12
sunilpar <sunilformal42@gmail.com>
2025-03-31 12:17:02 +0000

    merge sort

--- Algorithms/MergeSort.go
+++ Algorithms/MergeSort.go
@@ -4,11 +4,50 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-func divide[T constraints.Ordered](arr []T, p, q int) int {
+// Merge function to merge two sorted subarrays
+func merge[T constraints.Ordered](arr []T, left, mid, right int) {
+	leftSize := mid - left + 1
+	rightSize := right - mid
 
-	return 0
+	// Create temp slices
+	L := make([]T, leftSize)
+	R := make([]T, rightSize)
+
+	// Copy data
+	copy(L, arr[left:mid+1])
+	copy(R, arr[mid+1:right+1])
+
+	i, j, k := 0, 0, left
+	for i < leftSize && j < rightSize {
+		if L[i] <= R[j] {
+			arr[k] = L[i]
+			i++
+		} else {
+			arr[k] = R[j]
+			j++
+		}
+		k++
+	}
+
+	// Copy remaining elements
+	for i < leftSize {
+		arr[k] = L[i]
+		i++
+		k++
+	}
+	for j < rightSize {
+		arr[k] = R[j]
+		j++
+		k++
+	}
 }
 
-func MergeSort[T constraints.Ordered](arr []T, p, q int) {
-
+// MergeSort function
+func MergeSort[T constraints.Ordered](arr []T, left, right int) {
+	if left < right {
+		mid := left + (right-left)/2
+		MergeSort(arr, left, mid)
+		MergeSort(arr, mid+1, right)
+		merge(arr, left, mid, right)
+	}
 }
