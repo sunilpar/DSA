c280270ad76cf37709270b23536b128df61a2a28
sunilpar <sunilformal42@gmail.com>
2025-04-02 01:57:56 +0000

    testing binary search on 10 , 100, 1000, 10000 array

--- main.go
+++ main.go
@@ -7,18 +7,68 @@ import (
 )
 
 func main() {
-	arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
+	// arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
 	// arr1 := []int{80, 70, 60, 50, 40, 30, 20, 10}
 	// arr1 := rand.Perm(10)
 
+	arr1 := make([]int, 10)
+	for i := range arr1 {
+		arr1[i] = i
+	}
+
 	fmt.Print("binary search ")
 	s := time.Now()
-	v := Algo.Binarysearch(arr1, 0, len(arr1)-1, 10)
+	v := Algo.Binarysearch(arr1, 0, len(arr1)-1, 9)
 	if v == -1 {
 		fmt.Print("no element found\n")
 	} else {
 		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
 	}
 	e := time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = make([]int, 100)
+	for i := range arr1 {
+		arr1[i] = i
+	}
+
+	s = time.Now()
+	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 90)
+	if v == -1 {
+		fmt.Print("no element found\n")
+	} else {
+		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
+	}
+	e = time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = make([]int, 1000)
+	for i := range arr1 {
+		arr1[i] = i
+	}
+
+	s = time.Now()
+	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 900)
+	if v == -1 {
+		fmt.Print("no element found\n")
+	} else {
+		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
+	}
+	e = time.Since(s)
+	fmt.Printf("time taken:%v\n", e)
+
+	arr1 = make([]int, 10000)
+	for i := range arr1 {
+		arr1[i] = i
+	}
+
+	s = time.Now()
+	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 9000)
+	if v == -1 {
+		fmt.Print("no element found\n")
+	} else {
+		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
+	}
+	e = time.Since(s)
 	fmt.Printf("time taken:%v\n", e)
 }
