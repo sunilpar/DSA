cc9d8ffab2d00d36191e69d359e24f6d7fb6554c
sunilpar <sunilformal42@gmail.com>
2025-04-07 09:56:23 +0000

    written main program for maze solver

--- Algorithms/Maze.go
+++ Algorithms/Maze.go
@@ -1,1 +1,56 @@
 package Algo
+
+//maze solving algo
+
+func walk(maze [][]string, wall string, curr, end []int, seen [][]bool, path [][]int) bool {
+	dir := [][]int{
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{0, -1},
+	}
+	if curr[0] < 0 || curr[0] >= len(maze[0]) ||
+		curr[1] < 0 || curr[0] >= len(maze) {
+		return false
+	}
+	if maze[curr[0]][curr[1]] == wall {
+		return false
+	}
+	if end[0] == curr[0] && end[1] == curr[1] {
+		path = append(path, end)
+		return true
+	}
+	if seen[curr[0]][curr[1]] {
+		return false
+	}
+
+	//push
+	seen[curr[0]][curr[1]] = true
+	path = append(path, curr)
+
+	//recurse
+	for i := range len(dir) {
+		d := dir[i]
+		ncurr := curr
+		ncurr[0] = curr[0] + d[0]
+		ncurr[1] = curr[1] + d[1]
+		if walk(maze, wall, ncurr, end, seen, path) {
+			return true
+		}
+	}
+
+	//pop
+	path = path[:len(path)-1]
+
+	return false
+}
+
+func SolveMaze(maze [][]string, wall string, start, end []int) (path [][]int) {
+	seen := make([][]bool, len(maze))
+	for i := range seen {
+		seen[i] = make([]bool, len(maze[0]))
+	}
+	walk(maze, wall, start, end, seen, path)
+
+	return path
+}
--- main.go
+++ main.go
@@ -3,72 +3,19 @@ package main
 import (
 	Algo "DSA/Algorithms"
 	"fmt"
-	"time"
 )
 
 func main() {
-	// arr1 := []int{10, 20, 30, 40, 50, 60, 70, 80}
-	// arr1 := []int{80, 70, 60, 50, 40, 30, 20, 10}
-	// arr1 := rand.Perm(10)
-
-	arr1 := make([]int, 10)
-	for i := range arr1 {
-		arr1[i] = i
+	maze := [][]string{
+		{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"},
+		{"x", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", "x"},
+		{"x", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", "x"},
+		{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"},
+		{"x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x"},
+		{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
 	}
 
-	fmt.Print("binary search ")
-	s := time.Now()
-	v := Algo.Binarysearch(arr1, 0, len(arr1)-1, 9)
-	if v == -1 {
-		fmt.Print("no element found\n")
-	} else {
-		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
-	}
-	e := time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
+	path := Algo.SolveMaze(maze, "X", []int{0, 10}, []int{5, 1})
+	fmt.Println(path)
 
-	arr1 = make([]int, 100)
-	for i := range arr1 {
-		arr1[i] = i
-	}
-
-	s = time.Now()
-	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 90)
-	if v == -1 {
-		fmt.Print("no element found\n")
-	} else {
-		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
-	}
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
-
-	arr1 = make([]int, 1000)
-	for i := range arr1 {
-		arr1[i] = i
-	}
-
-	s = time.Now()
-	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 900)
-	if v == -1 {
-		fmt.Print("no element found\n")
-	} else {
-		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
-	}
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
-
-	arr1 = make([]int, 10000)
-	for i := range arr1 {
-		arr1[i] = i
-	}
-
-	s = time.Now()
-	v = Algo.Binarysearch(arr1, 0, len(arr1)-1, 9000)
-	if v == -1 {
-		fmt.Print("no element found\n")
-	} else {
-		fmt.Printf("element found at:arr[%v]:%v\n", v, arr1[v])
-	}
-	e = time.Since(s)
-	fmt.Printf("time taken:%v\n", e)
 }
