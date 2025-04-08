58a69d52d0a8a358ba37aceec6cdfa821cd2fde4
sunilpar <sunilformal42@gmail.com>
2025-04-08 03:07:01 +0000

    had to change return on the walk function everthing works now

--- Algorithms/Maze.go
+++ Algorithms/Maze.go
@@ -1,11 +1,7 @@
 package Algo
 
-import "fmt"
-
-//maze solving algo
-
-func walk(maze [][]string, wall string, curr, end []int, seen [][]bool, path [][]int) bool {
-	// fmt.Printf("called walk with : curr:%v end:%v path:%v\n", curr, end, path)
+// maze solving algo
+func walk(maze [][]string, wall string, curr, end []int, seen [][]bool, path [][]int) ([][]int, bool) {
 	dir := [][]int{
 		{-1, 0},
 		{0, 1},
@@ -14,46 +10,35 @@ func walk(maze [][]string, wall string, curr, end []int, seen [][]bool, path [][
 	}
 	if curr[0] < 0 || curr[1] >= len(maze[0]) ||
 		curr[1] < 0 || curr[0] >= len(maze) {
-		fmt.Printf("curr[0]:%v<0 || curr[1]:%v >=lenmaze[0]:%v\n", curr[0], curr[1], len(maze[0]))
-		fmt.Printf("curr[1]:%v<0 || curr[0]:%v >=lenmaze:%v\n", curr[1], curr[0], len(maze))
-		return false
+		return path, false
 	}
 	if maze[curr[0]][curr[1]] == wall {
-		fmt.Printf("inside wall curr[0]:%v curr[1]:%v\n", curr[0], curr[0])
-		return false
+		return path, false
 	}
 	if end[0] == curr[0] && end[1] == curr[1] {
-		fmt.Printf(" end !!!!!! should return true curr[0]:%v curr[1]:%v\n", curr[0], curr[1])
 		path = append(path, end)
-		fmt.Printf("\npath:%v\n", path)
-		return true
+		return path, true
 	}
 	if seen[curr[0]][curr[1]] {
-		fmt.Printf("inside seen curr[0]:%v curr[1]:%v\n", curr[0], curr[1])
-		return false
+		return path, false
 	}
 
-	//push
+	// push
 	seen[curr[0]][curr[1]] = true
 	path = append(path, curr)
 
-	//recurse
-	for i := range len(dir) {
-		d := dir[i]
-		ncurr := []int{0, 0}
-		ncurr[0] = curr[0] + d[0]
-		ncurr[1] = curr[1] + d[1]
-		fmt.Printf("curr:%v ncurr:%v\n", curr, ncurr)
-		if walk(maze, wall, ncurr, end, seen, path) {
-			return true
+	// recurse
+	for _, d := range dir {
+		ncurr := []int{curr[0] + d[0], curr[1] + d[1]}
+		updatedPath, found := walk(maze, wall, ncurr, end, seen, path)
+		if found {
+			return updatedPath, true
 		}
-		fmt.Println(" ")
 	}
 
-	//pop
+	// pop
 	path = path[:len(path)-1]
-
-	return false
+	return path, false
 }
 
 func SolveMaze(maze [][]string, wall string, start, end []int) (path [][]int) {
@@ -61,10 +46,9 @@ func SolveMaze(maze [][]string, wall string, start, end []int) (path [][]int) {
 	for i := range seen {
 		seen[i] = make([]bool, len(maze[0]))
 	}
-	if walk(maze, wall, start, end, seen, path) {
-		fmt.Printf("path:%v", path)
-		return path
+	finalPath, found := walk(maze, wall, start, end, seen, path)
+	if found {
+		return finalPath
 	}
-
 	return nil
 }
