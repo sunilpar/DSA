d82607cd4de33d5ca3e387e6cdb95c58e85c6b60
sunilpar <sunilformal42@gmail.com>
2025-04-08 03:26:57 +0000

    written multiple complex cases it works amazingly..

--- Tests/maze_test.go
+++ Tests/maze_test.go
@@ -50,6 +50,18 @@ func TestSolveMaze(t *testing.T) {
 			expected: true,
 		},
 		{
+			name: "No wall maze (open field)",
+			maze: [][]string{
+				{" ", " ", " "},
+				{" ", " ", " "},
+				{" ", " ", " "},
+			},
+			wall:     "x",
+			start:    []int{0, 0},
+			end:      []int{2, 2},
+			expected: true,
+		},
+		{
 			name: "Big maze with path",
 			maze: [][]string{
 				{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"},
@@ -65,15 +77,24 @@ func TestSolveMaze(t *testing.T) {
 			expected: true,
 		},
 		{
-			name: "No wall maze (open field)",
+			name: "big maze with complex path ", //made a 11 by 11 maze and made it complex as possible it needs to pop aaa lot lets test it
 			maze: [][]string{
-				{" ", " ", " "},
-				{" ", " ", " "},
-				{" ", " ", " "},
+				{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"},
+				{"x", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", "x"},
+				{"x", " ", " ", " ", " ", " ", " ", " ", " ", "x", " ", "x"},
+				{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", " ", "x"},
+				{"x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x"},
+				{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
+				{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
+				{"x", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "x"},
+				{"x", " ", "x", " ", " ", " ", " ", " ", " ", " ", " ", "x"},
+				{"x", " ", "x", " ", " ", " ", " ", " ", " ", " ", " ", "x"},
+				{"x", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
+				{" ", " ", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
 			},
 			wall:     "x",
-			start:    []int{0, 0},
-			end:      []int{2, 2},
+			start:    []int{0, 10},
+			end:      []int{11, 0},
 			expected: true,
 		},
 	}
@@ -81,7 +102,7 @@ func TestSolveMaze(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			path := Algo.SolveMaze(tt.maze, tt.wall, tt.start, tt.end)
-			got := len(path) > 0 && reflect.DeepEqual(path[len(path)-1], tt.end)
+			got := len(path) > 0 && reflect.DeepEqual(path[len(path)-1], tt.end) //reflect.deeepeual checks the last value of slice with end to ensure path is correct
 			if got != tt.expected {
 				t.Errorf("expected path found: %v, got: %v, path: %v", tt.expected, got, path)
 			}
