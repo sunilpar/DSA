c410b304d8ad797eb819a5c290ed318ac89c48b3
sunilpar <sunilformal42@gmail.com>
2025-04-15 05:49:30 +0000

    bug:compare graph funtion gives all as true need to dbug

--- Algorithms/GraphComparison.go
+++ Algorithms/GraphComparison.go
@@ -49,38 +49,55 @@ func (q *queue[T]) dequeue2() (*DS.GraphNode[T], bool) {
 	return elem.Value.(*DS.GraphNode[T]), true
 }
 
-// func CompareGraphs[T comparable](a *DS.Graph[T], b *DS.Graph[T]) (bool, error) {
-// 	if a.Nodes.Size() == 0 || b.Nodes.Size() == 0 {
-// 		return false, fmt.Errorf("graphs are empty!! \n")
-// 	}
-// 	head1 := a.Nodes.Head.Value
-// 	head2 := b.Nodes.Head.Value
-// 	q1 := newQueue1[T]()
-// 	q2 := newQueue2[T]()
-// 	q1.enqueue1(head1)
-// 	q2.enqueue2(head2)
-//
-// 	e1 := q1.list.Front()
-// 	e2 := q2.list.Front()
-// 	for {
-// 		if e != q.list.Front() {
-// 			n, err := q.dequeue()
-// 			if err == false {
-// 				log.Fatalf("coundn't dequeue!!!\n")
-// 			}
-// 			path = append(path, n)
-// 		}
-// 		node := e.Value.(*DS.GraphNode[T])
-// 		if node.Value == key {
-// 			return node, path, nil
-// 		} else {
-// 			for _, c := range node.Childs {
-// 				q.enqueue(c)
-// 			}
-// 		}
-//
-// 		e1 = e1.Next()
-// 		e2 = e2.Next()
-// 	}
-// 	return nil, nil, fmt.Errorf("coudn't find %+v\n", key)
-// }
+func CompareGraphs[T comparable](a *DS.Graph[T], b *DS.Graph[T]) (bool, error) {
+	if a.Nodes.Size() == 0 || b.Nodes.Size() == 0 {
+		return false, fmt.Errorf("graphs are empty!! \n")
+	}
+	head1 := a.Nodes.Head.Value
+	head2 := b.Nodes.Head.Value
+	q1 := newQueue1[T]()
+	q2 := newQueue2[T]()
+	q1.enqueue1(head1)
+	q2.enqueue2(head2)
+
+	e1 := q1.list.Front()
+	e2 := q2.list.Front()
+	for {
+		if e1 == nil && e2 == nil {
+			break
+		} else if e1 == nil || e2 == nil {
+			return false, nil
+		}
+
+		if e1 != q1.list.Front() && e2 != q2.list.Front() {
+			n1, err := q1.dequeue1()
+			if err == false {
+				log.Fatalf("coundn't dequeue!!!:%v\n", err)
+			}
+			n2, err := q2.dequeue2()
+			if err == false {
+				log.Fatalf("coundn't dequeue!!!:%v\n", err)
+			}
+			if n1.Value != n2.Value {
+				return false, nil
+			}
+		} else {
+			if e1.Value.(*DS.GraphNode[T]).Value != e2.Value.(*DS.GraphNode[T]).Value {
+				return false, nil
+			}
+		}
+
+		node1 := e1.Value.(*DS.GraphNode[T])
+		node2 := e2.Value.(*DS.GraphNode[T])
+		for _, c := range node1.Childs {
+			q1.enqueue(c)
+		}
+		for _, c := range node2.Childs {
+			q2.enqueue(c)
+		}
+
+		e1 = e1.Next()
+		e2 = e2.Next()
+	}
+	return true, nil
+}
--- main.go
+++ main.go
@@ -21,17 +21,29 @@ func main() {
 	if err != nil {
 		log.Fatalf("error: %s\n", err.Error())
 	}
-	f, p, err := Algo.BFS(g, 2)
+
+	g.Display()
+
+	h := DS.CreateGraph[int]()
+	h.Root(1)
+	h.Insertafter(1, 2)
+	h.Insertafter(1, 3)
+	h.Insertafter(2, 4)
+	h.Insertafter(2, 5)
+	h.Insertafter(3, 6)
+	h.Insertafter(3, 8)
+
+	err = h.Delete(2)
 	if err != nil {
 		log.Fatalf("error: %s\n", err.Error())
 	}
-	fmt.Printf("path:")
-	for _, v := range p {
-		fmt.Printf("%v ", v.Value)
-	}
-	fmt.Print("\n")
-	fmt.Printf("f.value:%v\n", f.Value)
 
-	g.Display()
+	h.Display()
+
+	s, err := Algo.CompareGraphs(g, h)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	fmt.Printf("\nis the graph same:%v\n", s)
 
 }
