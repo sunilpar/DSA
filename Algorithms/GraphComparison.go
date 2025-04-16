bf8f23b331e72782c697569dcfa08fd703d4e41e
sunilpar <sunilformal42@gmail.com>
2025-04-16 01:30:17 +0000

    redundant code removed from graphcomparison

--- Algorithms/GraphComparison.go
+++ Algorithms/GraphComparison.go
@@ -2,49 +2,9 @@ package Algo
 
 import (
 	DS "DSA/DataStructures"
-	"container/list"
 	"fmt"
 	"log"
 )
-
-type queue1[T comparable] struct {
-	list *list.List
-}
-
-type queue2[T comparable] struct {
-	list *list.List
-}
-
-func newQueue1[T comparable]() *queue[T] {
-	return &queue[T]{list: list.New()}
-}
-func newQueue2[T comparable]() *queue[T] {
-	return &queue[T]{list: list.New()}
-}
-
-func (q *queue[T]) enqueue1(v *DS.GraphNode[T]) {
-	q.list.PushBack(v)
-}
-func (q *queue[T]) enqueue2(v *DS.GraphNode[T]) {
-	q.list.PushBack(v)
-}
-
-func (q *queue[T]) dequeue1() (*DS.GraphNode[T], bool) {
-	if q.list.Len() == 0 {
-		return nil, false
-	}
-	elem := q.list.Front()
-	q.list.Remove(elem)
-	return elem.Value.(*DS.GraphNode[T]), true
-}
-func (q *queue[T]) dequeue2() (*DS.GraphNode[T], bool) {
-	if q.list.Len() == 0 {
-		return nil, false
-	}
-	elem := q.list.Front()
-	q.list.Remove(elem)
-	return elem.Value.(*DS.GraphNode[T]), true
-}
 
 func CompareGraphs[T comparable](a *DS.Graph[T], b *DS.Graph[T]) (bool, error) {
 	if a.Nodes.Size() == 0 || b.Nodes.Size() == 0 {
@@ -52,20 +12,20 @@ func CompareGraphs[T comparable](a *DS.Graph[T], b *DS.Graph[T]) (bool, error) {
 	}
 	head1 := a.Nodes.Head.Value
 	head2 := b.Nodes.Head.Value
-	q1 := newQueue1[T]()
-	q2 := newQueue2[T]()
-	q1.enqueue1(head1)
-	q2.enqueue2(head2)
+	q1 := newQueue[T]()
+	q2 := newQueue[T]()
+	q1.enqueue(head1)
+	q2.enqueue(head2)
 
 	e1 := q1.list.Front()
 	e2 := q2.list.Front()
 	for {
 		if e1 != q1.list.Front() && e2 != q2.list.Front() {
-			n1, err := q1.dequeue1()
+			n1, err := q1.dequeue()
 			if err == false {
 				log.Fatalf("coundn't dequeue!!!:%v\n", err)
 			}
-			n2, err := q2.dequeue2()
+			n2, err := q2.dequeue()
 			if err == false {
 				log.Fatalf("coundn't dequeue!!!:%v\n", err)
 			}
