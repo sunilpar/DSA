501438748426516241f648d94964c74e0a78b8f7
sunilpar <sunilformal42@gmail.com>
2025-04-17 01:49:53 +0000

    written insert for binary tree

--- DataStructures/Binarytree.go
+++ DataStructures/Binarytree.go
@@ -1,5 +1,73 @@
 package DS
 
+import "fmt"
+
 //binary tree with left and right as children
 //binarytreenode as node type
 //need to write search insertion deletion display with dfs
+
+type BinaryNode[T any] struct {
+	Value T
+	Right *BinaryNode[T]
+	Left  *BinaryNode[T]
+}
+
+type Btree[T comparable] struct {
+	Nodes *LinkedList[*BinaryNode[T]]
+}
+
+func CreateBtree[T comparable]() *Btree[T] {
+	return &Btree[T]{
+		Nodes: &LinkedList[*BinaryNode[T]]{},
+	}
+}
+
+func (g *Btree[T]) Root(value T) error {
+	if g.Nodes.Size() != 0 {
+		return fmt.Errorf("can't add root on non empty graph size of graph:%v ", g.Nodes.Size())
+	}
+	node := &BinaryNode[T]{Value: value}
+	g.Nodes.InsertAtFront(node)
+	return nil
+}
+
+func (g *Btree[T]) InsertRight(root T, value T) error {
+	newNode := &BinaryNode[T]{Value: value}
+	gn := &BinaryNode[T]{}
+	for e := g.Nodes.Head; e != nil; e = e.Next {
+		if e.Value.Value == root {
+			gn = e.Value
+			g.Nodes.Insert(newNode)
+			g.Nodes.Tail.Prev = e
+			gn.Right = newNode
+			return nil
+		}
+	}
+	return fmt.Errorf("unknown parent node :%v\n", root)
+
+}
+
+func (g *Btree[T]) InsertLeft(root T, value T) error {
+	newNode := &BinaryNode[T]{Value: value}
+	gn := &BinaryNode[T]{}
+	for e := g.Nodes.Head; e != nil; e = e.Next {
+		if e.Value.Value == root {
+			gn = e.Value
+			g.Nodes.Insert(newNode)
+			g.Nodes.Tail.Prev = e
+			gn.Left = newNode
+			return nil
+		}
+	}
+	return fmt.Errorf("unknown parent node :%v\n", root)
+
+}
+func (g *Btree[T]) Display() {
+	fmt.Print("\nnodes graphs:vvv\n")
+	for e := g.Nodes.Head; e != nil; e = e.Next {
+		fmt.Printf("%v", e.Value.Value)
+		fmt.Printf("->%+v ", e)
+		fmt.Printf("%p", e)
+		fmt.Printf("\n")
+	}
+}
--- main.go
+++ main.go
@@ -1,48 +1,45 @@
 package main
 
 import (
-	Algo "DSA/Algorithms"
 	DS "DSA/DataStructures"
-	"fmt"
 	"log"
 )
 
 func main() {
-	g := DS.CreateGraph[int]()
-	g.Root(1)
-	g.Insertafter(1, 2)
-	g.Insertafter(1, 3)
-	g.Insertafter(2, 4)
-	g.Insertafter(2, 5)
-	g.Insertafter(3, 6)
-	g.Insertafter(3, 7)
-
-	err := g.Delete(2)
+	g := DS.CreateBtree[int]()
+	err := g.Root(1)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertLeft(1, 2)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	err = g.InsertLeft(0, 2)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertLeft(2, 4)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertRight(2, 5)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertRight(1, 3)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertLeft(3, 6)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
+	g.InsertRight(3, 7)
 	if err != nil {
 		log.Fatalf("error: %s\n", err.Error())
 	}
 
 	g.Display()
-
-	h := DS.CreateGraph[int]()
-	h.Root(1)
-	h.Insertafter(1, 2)
-	h.Insertafter(1, 3)
-	h.Insertafter(2, 4)
-	h.Insertafter(2, 5)
-	h.Insertafter(3, 6)
-	h.Insertafter(3, 7)
-
-	err = h.Delete(2)
-	if err != nil {
-		log.Fatalf("error: %s\n", err.Error())
-	}
-	h.Display()
-
-	s, err := Algo.CompareGraphs(g, h)
-	if err != nil {
-		log.Fatalf("error: %s\n", err.Error())
-	}
-	fmt.Printf("\nis the graph same:%v\n", s)
 
 }
