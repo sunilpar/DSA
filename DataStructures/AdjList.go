dd3609e22d5202869e7a95d9190ade66b6634d33
sunilpar <sunilformal42@gmail.com>
2025-05-01 03:39:13 +0000

    written addnode and rootlist functions to add root and nodes

--- DataStructures/AdjList.go
+++ DataStructures/AdjList.go
@@ -1,5 +1,13 @@
 package DS
 
+import (
+	"fmt"
+	"log"
+)
+
+type Adjlist[T any] struct {
+	Nodes []ListNode[T]
+}
 type ListNode[T any] struct {
 	Value  T
 	Childs []*ListEdge[T]
@@ -10,10 +18,27 @@ type ListEdge[T any] struct {
 	Weight int
 }
 
-func CreateAdjList[T comparable]() *ListNode[T] {
-	return &ListNode[T]{}
+func CreateAdjList[T comparable]() *Adjlist[T] {
+	return &Adjlist[T]{}
 }
 
-func (g *ListNode[T]) RootList(value T) error {
+func (g *Adjlist[T]) rootList(value T) error {
+	if len(g.Nodes) != 0 {
+		return fmt.Errorf("graph already has other nodes!!")
+	}
+	r := ListNode[T]{Value: value}
+	g.Nodes = append(g.Nodes, r)
+	return nil
+}
+
+func (g *Adjlist[T]) AddNode(value T) error {
+	if len(g.Nodes) == 0 {
+		err := g.rootList(value)
+		if err != nil {
+			log.Fatalf("error: %s\n", err.Error())
+		}
+	}
+	n := ListNode[T]{Value: value}
+	g.Nodes = append(g.Nodes, n)
 	return nil
 }
--- main.go
+++ main.go
@@ -2,21 +2,11 @@ package main
 
 import (
 	DS "DSA/DataStructures"
-	"log"
 )
 
 func main() {
-	g := DS.CreateGraph[string]()
-	g.Root("sunil")
-	g.Insertafter("sunil", "rhand")
-	g.Insertafter("sunil", "lhand")
-	g.Insertafter("sunil", "body")
-
-	err := g.Delete("body")
-	if err != nil {
-		log.Fatalf("error: %s\n", err.Error())
-	}
-
-	g.Display()
-
+	g := DS.CreateAdjList[int]()
+	g.AddNode(20)
+	g.AddNode(30)
+	g.AddNode(10)
 }
