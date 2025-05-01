caa504d65b0f90e61d63eef4c7e51ebb29500d4d
sunilpar <sunilformal42@gmail.com>
2025-05-01 04:05:15 +0000

    wrote addedge funtion that adds edge and weight from parent to child node

--- DataStructures/AdjList.go
+++ DataStructures/AdjList.go
@@ -5,16 +5,16 @@ import (
 	"log"
 )
 
-type Adjlist[T any] struct {
+type Adjlist[T comparable] struct {
 	Nodes []ListNode[T]
 }
-type ListNode[T any] struct {
+type ListNode[T comparable] struct {
 	Value  T
-	Childs []*ListEdge[T]
+	Childs []ListEdge[T]
 }
 
-type ListEdge[T any] struct {
-	To     T
+type ListEdge[T comparable] struct {
+	To     int
 	Weight int
 }
 
@@ -40,5 +40,28 @@ func (g *Adjlist[T]) AddNode(value T) error {
 	}
 	n := ListNode[T]{Value: value}
 	g.Nodes = append(g.Nodes, n)
+	return nil
+}
+
+func (g *Adjlist[T]) AddEdge(p, c T, w int) error {
+	pi := -1
+	ci := -1
+	for i, v := range g.Nodes {
+		if v.Value == p {
+			pi = i
+		}
+		if v.Value == c {
+			ci = i
+		}
+		if ci != -1 && pi != -1 {
+			break
+		}
+	}
+	if ci == -1 || pi == -1 {
+		return fmt.Errorf("coundn't find child and parent :%v %v", pi, ci)
+	}
+	C := ListEdge[T]{To: ci, Weight: w}
+	g.Nodes[pi].Childs = append(g.Nodes[pi].Childs, C)
+	fmt.Printf("p:%v \n", g.Nodes[pi])
 	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	DS "DSA/DataStructures"
+	"log"
 )
 
 func main() {
 	g := DS.CreateAdjList[int]()
 	g.AddNode(20)
 	g.AddNode(30)
+	err := g.AddEdge(20, 30, 0)
+	if err != nil {
+		log.Fatalf("error: %s\n", err.Error())
+	}
 	g.AddNode(10)
 }
