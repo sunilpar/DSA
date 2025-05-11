750c93f288e3ef5f907febd7d5270b7e90efd005
sunilpar <sunilformal42@gmail.com>
2025-05-11 14:15:43 +0000

    written algo for search insert position

---
+++ Leetcode/arrays/searchInsert.go
@@ -0,0 +1,9 @@
+package leetcode
+
+func searchInsert(nums []int, target int) int {
+	// binary search and
+	// after search if not found
+	// if nums[mid]<target
+	// return mid+1
+	// else mid-1
+}
