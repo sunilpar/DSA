2dce56f524c11d92a3f9fba4360568f5efd0fc70
sunilpar <sunilformal42@gmail.com>
2025-05-14 14:17:24 +0000

    written somewhat optimized solution for 3sum problem in n3 time

--- Leetcode/arrays/3sums.go
+++ Leetcode/arrays/3sums.go
@@ -1,11 +1,32 @@
 package leetcode
 
-func ThreeSum(nums []int) [][]int {
-	//seem array of nums size and true if index consitues the solution
-	//same brut force approch first
-	//3 loops
-	//i to find i index
-	//j and k to do the same as well
+import "sort"
 
-	//append when condition matches and return the array at the end
+func ThreeSum(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	n := len(nums)
+	for i := 0; i < n-2 && nums[i] <= 0; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j, k := i+1, n-1
+		for j < k {
+			x := nums[i] + nums[j] + nums[k]
+			if x < 0 {
+				j++
+			} else if x > 0 {
+				k--
+			} else {
+				ans = append(ans, []int{nums[i], nums[j], nums[k]})
+				j, k = j+1, k-1
+				for j < k && nums[j] == nums[j-1] {
+					j++
+				}
+				for j < k && nums[k] == nums[k+1] {
+					k--
+				}
+			}
+		}
+	}
+	return
 }
