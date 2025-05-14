acf0bde0f3a104348cc908961145b211c1ff8d83
sunilpar <sunilformal42@gmail.com>
2025-05-14 14:00:38 +0000

    written brut force solution for 3 sum problem n3 time

---
+++ Leetcode/arrays/3sums.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func ThreeSum(nums []int) [][]int {
+	//seem array of nums size and true if index consitues the solution
+	//same brut force approch first
+	//3 loops
+	//i to find i index
+	//j and k to do the same as well
+
+	//append when condition matches and return the array at the end
+}
