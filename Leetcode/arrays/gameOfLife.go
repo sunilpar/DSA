d1741c445e3ea9bb34dbb6ce5793d78e0b28cf91
sunilpar <sunilformal42@gmail.com>
2025-05-15 04:05:03 +0000

    written condition for conway game of life

---
+++ Leetcode/arrays/gameOfLife.go
@@ -0,0 +1,13 @@
+package leetcode
+
+//@NOTE: Conditions
+//@NOTE: 1 ALIVE
+//@NOTE: nev keep 1 if 2 , 3 1's
+//@NOTE: put 0 if 4 1's or  0 to 1 1's
+
+//@NOTE: 0 DEAD
+//@NOTE: put 1 if nev has 3 1's
+
+func gameOfLife(board [][]int) {
+
+}
