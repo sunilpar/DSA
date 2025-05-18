c9a75212e22a35fec9dcab8ddc2c2ee2b88e8333
sunilpar <sunilformal42@gmail.com>
2025-05-18 15:00:47 +0000

    wrote brut solution for conway game of life

--- Leetcode/arrays/gameOfLife.go
+++ Leetcode/arrays/gameOfLife.go
@@ -1,13 +1,5 @@
 package leetcode
 
-//@NOTE: Conditions
-//@NOTE: 1 ALIVE
-//@NOTE: nev keep 1 if 2 , 3 1's
-//@NOTE: put 0 if 4 1's or  0 to 1 1's
-
-//@NOTE: 0 DEAD
-//@NOTE: put 1 if nev has 3 1's
-
 func GameOfLife(board [][]int) {
 	org := []int{}
 	for i, _ := range board {
