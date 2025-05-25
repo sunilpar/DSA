e0f2d1bd155979cf91720279f4a11a4f7d5408c4
sunilpar <sunilformal42@gmail.com>
2025-05-25 14:40:06 +0000

    _

--- Leetcode/arrays/medianOfSortedArr.go
+++ Leetcode/arrays/medianOfSortedArr.go
@@ -2,9 +2,4 @@ package leetcode
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
-	//sort the arrays into one
-	//however doing this sorted in 0(log(m+n)) time is the biggest challange
-	//havent figure out that yet
-	//find the median
-
 }
