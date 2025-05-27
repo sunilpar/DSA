2764a0a7d3a69122f9505490ca47a19bf76c1c56
sunilpar <sunilformal42@gmail.com>
2025-05-27 13:42:51 +0000

    done in constant time What...

--- Leetcode/arrays/medianOfSortedArr.go
+++ Leetcode/arrays/medianOfSortedArr.go
@@ -1,6 +1,7 @@
 package leetcode
 
 func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+
 	m, n := len(nums1), len(nums2)
 	var f func(i, j, k int) int
 	f = func(i, j, k int) int {
@@ -28,4 +29,5 @@ func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	}
 	a, b := f(0, 0, (m+n+1)/2), f(0, 0, (m+n+2)/2)
 	return float64(a+b) / 2.0
+
 }
