41b5c6eff7887ca8301f4f9506e5d7ad1c4ef66b
sunilpar <sunilformal42@gmail.com>
2025-05-27 02:51:56 +0000

    works correctly

--- Leetcode/arrays/medianOfSortedArr.go
+++ Leetcode/arrays/medianOfSortedArr.go
@@ -1,6 +1,6 @@
 package leetcode
 
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	m, n := len(nums1), len(nums2)
 	var f func(i, j, k int) int
 	f = func(i, j, k int) int {
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	leetcode "DSA/Leetcode/arrays"
+	"fmt"
 )
 
 func main() {
-	board := [][]int{{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}}
-	leetcode.GameOfLife(board)
+	nums1 := []int{1, 2, 4}
+	nums2 := []int{3}
+	m := leetcode.FindMedianSortedArrays(nums1, nums2)
+	fmt.Printf("median:%v\n", m)
 }
