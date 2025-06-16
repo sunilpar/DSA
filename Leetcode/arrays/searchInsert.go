6debdde966678b46d6ec35939c65493a99624062
sunilpar <sunilformal42@gmail.com>
2025-06-16 14:04:05 +0000

    _

--- Leetcode/arrays/searchInsert.go
+++ Leetcode/arrays/searchInsert.go
@@ -3,7 +3,6 @@ package leetcode
 import "fmt"
 
 func SearchInsert(nums []int, target int) int {
-
 	l := 0
 	h := len(nums) - 1
 	mid := l + (h-l)/2
@@ -20,5 +19,4 @@ func SearchInsert(nums []int, target int) int {
 		mid = l + (h-l)/2
 	}
 	return mid
-
 }
