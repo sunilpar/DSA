4a570a44160d3a3bb3b1d1e6999abbc6a7880412
sunilpar <sunilformal42@gmail.com>
2025-07-16 15:42:21 +0000

    _

--- Leetcode/arrays/medianOfSortedArr.go
+++ Leetcode/arrays/medianOfSortedArr.go
@@ -1,7 +1,6 @@
 package leetcode
 
 func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
 	m, n := len(nums1), len(nums2)
 	var f func(i, j, k int) int
 	f = func(i, j, k int) int {
@@ -29,7 +28,6 @@ func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	}
 	a, b := f(0, 0, (m+n+1)/2), f(0, 0, (m+n+2)/2)
 	return float64(a+b) / 2.0
-
 }
 
 //https://leetcode.com/problems/median-of-two-sorted-arrays/description/?envType=problem-list-v2&envId=array
